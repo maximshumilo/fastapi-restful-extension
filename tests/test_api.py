52427cf94111da4cbf08e795589c0f5ac77196a2
Maksim Shumilo <m.shumilo@visionlabs.ru>
2022-05-22 23:45:33 +0300

    Update test

--- tests/test_api.py
+++ tests/test_api.py
@@ -39,3 +39,14 @@ def test_str_of_instance(rest_api_for_test):
     prefix = '/v1'
     api = rest_api_for_test.create_version(prefix)
     assert str(api) == f'API: {prefix}'
+
+
+def test_url_map(rest_api_for_test, resource_for_test):
+    def get(self, **kwargs):
+        return {}
+    resource_for_test.get = get
+    resource_prefix = '/test'
+    rest_api_for_test.add_resource(resource_for_test, resource_prefix)
+    base_prefix = rest_api_for_test.router.prefix
+    path = f'{base_prefix}{resource_prefix}'
+    assert rest_api_for_test.url_map.get(path) is not None
