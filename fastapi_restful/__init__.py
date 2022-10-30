2b1aad5e21b897d2052e0346c76aae0ba8efbabb
Maksim Shumilo <m.shumilo@visionlabs.ru>
2022-10-30 14:35:20 +0300

    Run formatters

--- fastapi_restful/__init__.py
+++ fastapi_restful/__init__.py
@@ -2,8 +2,4 @@ from .api import RestAPI
 from .resource import Resource
 from .utils import route_settings
 
-__all__ = [
-    'RestAPI',
-    'Resource',
-    'route_settings'
-]
+__all__ = ["RestAPI", "Resource", "route_settings"]
