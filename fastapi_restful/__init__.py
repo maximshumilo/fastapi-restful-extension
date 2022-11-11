1b1a1efa0fd3d2e77d51b785ebaed60f04e34316
Maksim Shumilo <m.shumilo@visionlabs.ru>
2022-11-11 15:57:14 +0300

    Update APIVersion

--- fastapi_restful/__init__.py
+++ fastapi_restful/__init__.py
@@ -1,5 +1,5 @@
-from .api import RestAPI
+from .api import RestAPI, APIVersion
 from .resource import Resource
 from .utils import route_settings
 
-__all__ = ["RestAPI", "Resource", "route_settings"]
+__all__ = ["RestAPI", "Resource", "route_settings", "APIVersion"]
